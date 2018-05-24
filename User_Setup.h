d134a7bd36f6efb55521635c0e8ebdeb31cf44ac
Pedro Albuquerque <pedro_m_albuquerque@hotmail.com>
2018-05-24 22:00:54 +0100

    ST7735 ON ESP32
    connected ITDB2_1.8SP to a ESP32 on pins 12,13,14,27 and 26.
    MISO pin defined on 25 but not used, but code complains if not defined...

--- User_Setup.h
+++ User_Setup.h
@@ -14,8 +14,8 @@
 // ##################################################################################
 
 // Only define one driver, the other ones must be commented out
-#define ILI9341_DRIVER
-//#define ST7735_DRIVER
+//#define ILI9341_DRIVER
+#define ST7735_DRIVER
 //#define ILI9163_DRIVER
 //#define S6D02A1_DRIVER
 //#define RPI_ILI9486_DRIVER // 20MHz maximum SPI
@@ -83,9 +83,9 @@
 // ###### EDIT THE PIN NUMBERS IN THE LINES FOLLOWING TO SUIT YOUR ESP8266 SETUP ######
 
 // For NodeMCU - use pin numbers in the form PIN_Dx where Dx is the NodeMCU pin designation
-#define TFT_CS   PIN_D8  // Chip select control pin D8
-#define TFT_DC   PIN_D3  // Data Command control pin
-#define TFT_RST  PIN_D4  // Reset pin (could connect to NodeMCU RST, see next line)
+//#define TFT_CS   PIN_D8  // Chip select control pin D8
+//#define TFT_DC   PIN_D3  // Data Command control pin
+//#define TFT_RST  PIN_D4  // Reset pin (could connect to NodeMCU RST, see next line)
 //#define TFT_RST  -1  // Set TFT_RST to -1 if the display RESET is connected to NodeMCU RST or 3.3V
 
 //#define TOUCH_CS PIN_D1     // Chip select pin (T_CS) of touch screen
@@ -109,12 +109,12 @@
 // For ESP32 Dev board (only tested with ILI9341 display)
 // The hardware SPI can be mapped to any pins
 
-//#define TFT_MISO 19
-//#define TFT_MOSI 23
-//#define TFT_SCLK 18
-//#define TFT_CS    15  // Chip select control pin
-//#define TFT_DC    2  // Data Command control pin
-//#define TFT_RST   4  // Reset pin (could connect to RST pin)
+#define TFT_MISO 25  //not used ??
+#define TFT_MOSI 14 //23
+#define TFT_SCLK 12 //18
+#define TFT_CS    13 //15  // Chip select control pin
+#define TFT_DC    27 //2  // Data Command control pin
+#define TFT_RST   26 //4  // Reset pin (could connect to RST pin)
 //#define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
 
 // For the M5Stack module use these #define lines
@@ -193,7 +193,7 @@
 // #define SPI_FREQUENCY  80000000
 
 // The XPT2046 requires a lower SPI clock rate of 2.5MHz so we define that here:
-#define SPI_TOUCH_FREQUENCY  2500000
+//#define SPI_TOUCH_FREQUENCY  2500000
 
 
 // Comment out the following #define if "SPI Transactions" do not need to be
